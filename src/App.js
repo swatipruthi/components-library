6e5692df9343d5981372b0e6ea567cc483d0c827
Swati Pruthi <swatipruthi@hotmail.com>
2019-11-23 03:08:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.scss"
+import {
+	Nav,
+	NavItem,
+	NavLink,
+	Dropdown,
+	DropdownItem,
+	DropdownToggle,
+	DropdownMenu
+} from "reactstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		value: "",
+		dropdownOpen: false
+	}
+
+	onChange = e => {
+		this.setState({ value: e.target.value })
+	}
+
+	toggle = () => this.setState({ dropdownOpen: !this.state.dropdownOpen })
+
+	renderSearchInput() {
+		return (
+			<div>
+				<input
+					type='search'
+					value={this.state.value}
+					placeholder='Search here...'
+					onChange={this.onChange}
+				/>
+				<span style={{ right: "30px", position: "relative", color: "black" }}>
+					<i className='fa fa-search'></i>
+				</span>
+			</div>
+		)
+	}
+
+	renderMultiLevelDropdown() {
+		return (
+			<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+				<DropdownToggle caret>Dropdown</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem disabled>Action</DropdownItem>
+					<DropdownItem>Another Action</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>Another Action</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+		)
+	}
+
+	render() {
+		return (
+			<div className='Container'>
+				<div className='App-body'>
+					<header className='Heading'>Components Library</header>
+					<Nav vertical>
+						<div className='p-5'>{this.renderSearchInput()}</div>
+						{this.renderMultiLevelDropdown()}
+						<hr />
+					</Nav>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
